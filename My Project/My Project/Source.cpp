6eca0bafb53b73682e055403be0fcb16f1ee41b0
Costin Vlad <costinvlad003@gmail.com>
2023-11-24 23:42:46 +0200

    Added the EventLocation class, constructors, the destructor and methods for displaying hte attributes.

--- My Project/My Project/Source.cpp
+++ My Project/My Project/Source.cpp
@@ -1,6 +1,65 @@
-#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
 
-int main()
+#include <iostream>
+#include <cstring>
+#include <ctime>
+
+class EventLocation {
+private:
+    int* seatsPerRow;
+    int maxSeats;
+    int numRows;
+    int numZones;
+
+public:
+    EventLocation(int maxSeats, int numRows, int numZones, const int* seatsPerRow);
+    ~EventLocation();
+
+    int getMaxSeats() const;
+    int getNumRows() const;
+    int getNumZones() const;
+    const int* getSeatsPerRow() const;
+
+    void display() const;
+};
+
+EventLocation::EventLocation(int maxSeats, int numRows, int numZones, const int* seatsPerRow)
+    : maxSeats(maxSeats), numRows(numRows), numZones(numZones)
 {
-	std::cout << std::endl << "Hello World!";
+    // Allocating memory for seatsPerRow and copying values
+    this->seatsPerRow = new int[numRows];
+    for (int i = 0; i < numRows; ++i) {
+        this->seatsPerRow[i] = seatsPerRow[i];
+    }
+}
+
+EventLocation::~EventLocation() {
+    delete[] seatsPerRow;
+}
+
+int EventLocation::getMaxSeats() const {
+    return maxSeats;
+}
+
+int EventLocation::getNumRows() const {
+    return numRows;
+}
+
+int EventLocation::getNumZones() const {
+    return numZones;
+}
+
+const int* EventLocation::getSeatsPerRow() const {
+    return seatsPerRow;
+}
+
+void EventLocation::display() const {
+    std::cout << "Event Location: Max Seats=" << maxSeats
+        << ", Num Rows=" << numRows
+        << ", Num Zones=" << numZones << std::endl;
+    std::cout << "Seats per Row: ";
+    for (int i = 0; i < numRows; ++i) {
+        std::cout << seatsPerRow[i] << " ";
+    }
+    std::cout << std::endl;
 }
