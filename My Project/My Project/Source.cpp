1363d6fc0cb61746db4319002f41c15746f29c9e
Costin Vlad <costinvlad003@gmail.com>
2024-01-09 22:27:16 +0200

    I added a derived class from Ticket called FootballTicket to handle the scenarios provided in the requirements

--- My Project/My Project/Source.cpp
+++ My Project/My Project/Source.cpp
@@ -4,26 +4,25 @@
 #include <cstring>
 #include <vector>
 
-class EventLocation {   //This is the EventLocation class representing the characteristics of the event location
+class EventLocation {
 private:
-    int* seatsPerRow; //A dynamically allocated array
+    int* seatsPerRow;
     int maxSeats;
-    int numRows; //Statically defined numerical values
+    int numRows;
     int numZones;
 
 public:
-    EventLocation(int maxSeats, int numRows, int numZones, const int* seatsPerRow); //Constructor for initializing the event location.
-    EventLocation(const EventLocation& other); //Copy constructor for creating a copy of an existing event location
-    ~EventLocation(); //Destructor for cleaning up dynamically allocated memory
+    EventLocation(int maxSeats, int numRows, int numZones, const int* seatsPerRow);
+    EventLocation(const EventLocation& other);
+    ~EventLocation();
 
     int getMaxSeats() const;
-    int getNumRows() const; //Accessor functions
+    int getNumRows() const;
     int getNumZones() const;
     const int* getSeatsPerRow() const;
 
-    void display() const; //Method to display the details of the event location
+    void display() const;
 
-    // Overloaded operators
     int operator[](int index) const;
     EventLocation& operator=(const EventLocation& other);
     friend std::ostream& operator<<(std::ostream& os, const EventLocation& location);
@@ -32,7 +31,7 @@ public:
 
 EventLocation::EventLocation(int maxSeats, int numRows, int numZones, const int* seatsPerRow)
     : maxSeats(maxSeats), numRows(numRows), numZones(numZones) {
-    this->seatsPerRow = new int[numRows]; //Allocating memory for seatsPerRow and copying values
+    this->seatsPerRow = new int[numRows];
     for (int i = 0; i < numRows; ++i) {
         this->seatsPerRow[i] = seatsPerRow[i];
     }
@@ -68,8 +67,8 @@ const int* EventLocation::getSeatsPerRow() const {
 
 void EventLocation::display() const {
     std::cout << "Event Location: Max Seats=" << maxSeats
-        << ", Num Rows=" << numRows
-        << ", Num Zones=" << numZones << std::endl;
+              << ", Num Rows=" << numRows
+              << ", Num Zones=" << numZones << std::endl;
     std::cout << "Seats per Row: ";
     for (int i = 0; i < numRows; ++i) {
         std::cout << seatsPerRow[i] << " ";
@@ -78,18 +77,16 @@ void EventLocation::display() const {
 }
 
 int EventLocation::operator[](int index) const {
-    if (index >= 0 && index < numRows) { //// Validation for out of bounds access
+    if (index >= 0 && index < numRows) {
         return seatsPerRow[index];
-    }
-    else {
-        
+    } else {
         std::cerr << "Error: Index out of bounds." << std::endl;
-        return -1;  
+        return -1;
     }
 }
 
 EventLocation& EventLocation::operator=(const EventLocation& other) {
-    if (this != &other) {  
+    if (this != &other) {
         maxSeats = other.maxSeats;
         numRows = other.numRows;
         numZones = other.numZones;
@@ -106,8 +103,8 @@ EventLocation& EventLocation::operator=(const EventLocation& other) {
 
 std::ostream& operator<<(std::ostream& os, const EventLocation& location) {
     os << "Event Location: Max Seats=" << location.maxSeats
-        << ", Num Rows=" << location.numRows
-        << ", Num Zones=" << location.numZones << std::endl;
+       << ", Num Rows=" << location.numRows
+       << ", Num Zones=" << location.numZones << std::endl;
     os << "Seats per Row: ";
     for (int i = 0; i < location.numRows; ++i) {
         os << location.seatsPerRow[i] << " ";
@@ -118,51 +115,37 @@ std::ostream& operator<<(std::ostream& os, const EventLocation& location) {
 
 bool EventLocation::operator==(const EventLocation& other) const {
     return maxSeats == other.maxSeats &&
-        numRows == other.numRows &&
-        numZones == other.numZones &&
-        std::equal(seatsPerRow, seatsPerRow + numRows, other.seatsPerRow);
+           numRows == other.numRows &&
+           numZones == other.numZones &&
+           std::equal(seatsPerRow, seatsPerRow + numRows, other.seatsPerRow);
 }
 
-// Definition of the Event class
 class Event {
+public:
+    static const int MAX_STRING_LENGTH = 50;
+
 private:
     char* eventName;
     char* eventDate;
     char* eventTime;
 
 public:
-    static const int MAX_STRING_LENGTH = 50;
-
-    // Constructor: initializes an Event with the given name, date, and time
     Event(const char* eventName, const char* eventDate, const char* eventTime);
-
-    // Copy constructor: creates a copy of another Event
     Event(const Event& other);
-
-    // Destructor: cleans up dynamically allocated memory
     ~Event();
 
-    // Getter methods for accessing event details
     const char* getEventName() const;
     const char* getEventDate() const;
     const char* getEventTime() const;
 
-    // Display method: Prints the details of the event
     void display() const;
 
-    // Overloaded assignment operator: assigns the values of another Event
     Event& operator=(const Event& other);
-
-    // Overloaded operator <<: allows printing an Event to an ostream
     friend std::ostream& operator<<(std::ostream& os, const Event& event);
-
-    // Overloaded  operator = : checks if two Events are equal
     bool operator==(const Event& other) const;
 };
 
-// Constructor implementation
 Event::Event(const char* eventName, const char* eventDate, const char* eventTime) {
-    // Allocating memory for the character arrays and copying values
     this->eventName = new char[MAX_STRING_LENGTH];
     this->eventDate = new char[MAX_STRING_LENGTH];
     this->eventTime = new char[MAX_STRING_LENGTH];
@@ -172,9 +155,7 @@ Event::Event(const char* eventName, const char* eventDate, const char* eventTime
     strncpy(this->eventTime, eventTime, MAX_STRING_LENGTH);
 }
 
-// Copy constructor implementation
 Event::Event(const Event& other) {
-   
     eventName = new char[MAX_STRING_LENGTH];
     eventDate = new char[MAX_STRING_LENGTH];
     eventTime = new char[MAX_STRING_LENGTH];
@@ -184,15 +165,12 @@ Event::Event(const Event& other) {
     strncpy(eventTime, other.eventTime, MAX_STRING_LENGTH);
 }
 
-// Destructor implementation
 Event::~Event() {
-    // Deallocating memory for the character arrays
     delete[] eventName;
     delete[] eventDate;
     delete[] eventTime;
 }
 
-// Getter methods
 const char* Event::getEventName() const {
     return eventName;
 }
@@ -205,16 +183,14 @@ const char* Event::getEventTime() const {
     return eventTime;
 }
 
-// Display methods 
 void Event::display() const {
     std::cout << "Event: Name=" << eventName
-        << ", Date=" << eventDate
-        << ", Time=" << eventTime << std::endl;
+              << ", Date=" << eventDate
+              << ", Time=" << eventTime << std::endl;
 }
 
-// Overloaded operator =
 Event& Event::operator=(const Event& other) {
-    if (this != &other) {  // Check for self-assignment
+    if (this != &other) {
         strncpy(eventName, other.eventName, MAX_STRING_LENGTH);
         strncpy(eventDate, other.eventDate, MAX_STRING_LENGTH);
         strncpy(eventTime, other.eventTime, MAX_STRING_LENGTH);
@@ -222,74 +198,53 @@ Event& Event::operator=(const Event& other) {
     return *this;
 }
 
-// Overloaded operator <<
 std::ostream& operator<<(std::ostream& os, const Event& event) {
     os << "Event: Name=" << event.eventName
-        << ", Date=" << event.eventDate
-        << ", Time=" << event.eventTime;
+       << ", Date=" << event.eventDate
+       << ", Time=" << event.eventTime;
     return os;
 }
 
-// Overloaded operator ==
 bool Event::operator==(const Event& other) const {
     return strncmp(eventName, other.eventName, MAX_STRING_LENGTH) == 0 &&
-        strncmp(eventDate, other.eventDate, MAX_STRING_LENGTH) == 0 &&
-        strncmp(eventTime, other.eventTime, MAX_STRING_LENGTH) == 0;
+           strncmp(eventDate, other.eventDate, MAX_STRING_LENGTH) == 0 &&
+           strncmp(eventTime, other.eventTime, MAX_STRING_LENGTH) == 0;
 }
 
-// Definition of the Ticket class
 class Ticket {
 private:
-    // Static member to track the next available ticket ID
     static int nextTicketId;
 
     int ticketId;
     char* ticketType;
 
 public:
-    // Constructor 
     Ticket(const char* ticketType);
-
-    // Destructor
     ~Ticket();
 
-    // Getter methods for accessing ticket details
     int getTicketId() const;
     const char* getTicketType() const;
 
-    // Display method
-    void display() const;
+    virtual void display() const = 0;
 
-    // Overloaded = operator
     Ticket& operator=(const Ticket& other);
-
-    // Overloaded stream insertion operator
     friend std::ostream& operator<<(std::ostream& os, const Ticket& ticket);
-
-    // Overloaded equality operator
     bool operator==(const Ticket& other) const;
 };
 
-// Initialize the static member for the next ticket ID
 int Ticket::nextTicketId = 1;
 
-// Constructor 
 Ticket::Ticket(const char* ticketType) {
-    // Allocating memory for the character array and copy ticketType
     this->ticketType = new char[Event::MAX_STRING_LENGTH];
     strncpy(this->ticketType, ticketType, Event::MAX_STRING_LENGTH);
 
-    // Assigning a unique ticket ID and incrementing the counter for the next ticket
     this->ticketId = nextTicketId++;
 }
 
-// Destructor 
 Ticket::~Ticket() {
-  
     delete[] ticketType;
 }
 
-// Getter method
 int Ticket::getTicketId() const {
     return ticketId;
 }
@@ -298,63 +253,66 @@ const char* Ticket::getTicketType() const {
     return ticketType;
 }
 
-// Display method 
 void Ticket::display() const {
     std::cout << "Ticket ID=" << ticketId << ", Type=" << ticketType << std::endl;
 }
 
-// Overloaded = operator 
 Ticket& Ticket::operator=(const Ticket& other) {
-    if (this != &other) {  // Check for self-assignment
-        // Copy ticketType from another Ticket
+    if (this != &other) {
         strncpy(ticketType, other.ticketType, Event::MAX_STRING_LENGTH);
     }
     return *this;
 }
 
-// Overloaded stream insertion operator 
 std::ostream& operator<<(std::ostream& os, const Ticket& ticket) {
     os << "Ticket ID=" << ticket.ticketId << ", Type=" << ticket.ticketType;
     return os;
 }
 
-// Overloaded equality operator 
 bool Ticket::operator==(const Ticket& other) const {
     return ticketId == other.ticketId &&
-        strncmp(ticketType, other.ticketType, Event::MAX_STRING_LENGTH) == 0;
+           strncmp(ticketType, other.ticketType, Event::MAX_STRING_LENGTH) == 0;
 }
 
+class FootballTicket : public Ticket {
+private:
+    int stand;
+    int row;
+    int seat;
+
+public:
+    FootballTicket(const char* ticketType, int stand, int row, int seat);
+    ~FootballTicket();
+
+    int getStand() const;
+    int getRow() const;
+    int getSeat() const;
+
+    void display() const override;
+};
+
+FootballTicket::FootballTicket(const char* ticketType, int stand, int row, int seat)
+    : Ticket(ticketType), stand(stand), row(row), seat(seat) {}
+
+FootballTicket::~FootballTicket() {}
+
+int FootballTicket::getStand() const {
+    return stand;
+}
+
+int FootballTicket::getRow() const {
+    return row;
+}
+
+int FootballTicket::getSeat() const {
+    return seat;
+}
+
+void FootballTicket::display() const {
+    std::cout << "Football Ticket - " << *this << ", Stand=" << stand
+              << ", Row=" << row << ", Seat=" << seat << std::endl;
+}
 
 int main() {
-     
-    int maxSeats = 100;
-    int numRows = 5;
-    int numZones = 2;
-    int* seatsPerRow = new int[numRows] {20, 20, 20, 20, 20};
-
-    EventLocation location(maxSeats, numRows, numZones, seatsPerRow);
-
-    std::cout << "Event Location Details:\n";
-    location.display();
-
-    const char eventName[] = "Movie Night";
-    const char eventDate[] = "2023-12-01";
-    const char eventTime[] = "19:30";
-
-    Event movieEvent(eventName, eventDate, eventTime);
-
-    std::cout << "\nEvent Details:\n";
-    movieEvent.display();
-
-    const char ticketType[] = "VIP";
-
-    Ticket vipTicket(ticketType);
-
-    std::cout << "\nGenerated Ticket Details:\n";
-    vipTicket.display();
-
-    
-    delete[] seatsPerRow;
-
     return 0;
 }
